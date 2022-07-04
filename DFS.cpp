d94bd3afb6515ba03610d1beb04f29425725e5d5
faizaan1904 <68111240+faizaan1904@users.noreply.github.com>
2022-07-04 14:23:50 +0530

    Create DFS.cpp
    Create Depth first search for graph

---
+++ DFS.cpp
@@ -0,0 +1,20 @@
+void dfs(int v, vector<int> adj[], vector<int>& ans, vector<bool>& vis){
+        
+        if(vis[v]) return;
+        
+        vis[v] = true;
+        ans.push_back(v);
+        
+        for(auto child : adj[v]){
+            dfs(child, adj, ans, vis);
+        }
+    }
+    
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> ans;
+        vector<bool> vis(V, false);
+        
+        dfs(0, adj, ans, vis);
+        
+        return ans;
+    }
