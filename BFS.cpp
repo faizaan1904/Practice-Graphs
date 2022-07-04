99c0543cb6733cb70bf2878bfb53a87df18c67df
faizaan1904 <68111240+faizaan1904@users.noreply.github.com>
2022-07-04 12:56:29 +0530

    Create BFS.cpp
    Code for breadth first search

---
+++ BFS.cpp
@@ -0,0 +1,23 @@
+vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+       bool travel[V+1];
+       vector<int>v;
+       queue<int>q;
+       for(int i =0 ; i< V ; i++){
+           travel[i]=false;
+       }
+       q.push(0);
+       travel[0]=true;
+       while(!q.empty()){
+           int x=q.front();
+           v.push_back(x);
+           q.pop();
+           for(auto i : adj[x]){
+               if(travel[i]==false){
+                   travel[i]=true;
+                   q.push(i);
+               }
+           }
+       }
+       return v;
+    }
